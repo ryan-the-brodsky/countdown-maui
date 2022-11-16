904581840db89017e8d558de3b9b783f20524739
Ryan Brodsky <ryan.d.brodsky@gmail.com>
2022-11-15 20:05:36 -0700

    start of generating random letters, word gets built but the layouts UI is buggy and keeps space where removed elements were after submitWord function gets called.

---
+++ CowTown/LetterUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CowTown
+{
+    public static class LetterUtils
+    {
+        private static Dictionary<char, int> difficultyIndex = new Dictionary<char, int>();
+        private static Random rand = new Random();
+
+        public static List<char> GiveSevenLetters()
+        {
+            int lettersAdded = 0;
+            int vowelsAdded = 0;
+            string vowels = "aeiou";
+            List<char> letterList = new List<char>();
+            
+            while(lettersAdded < 7)
+            {
+                char randomLetter = Convert.ToChar(rand.Next(65, 91));
+                letterList.Add(randomLetter);
+                lettersAdded += 1;
+            }
+            
+            return letterList;
+        }
+    }
+}
+
--- CowTown/MainPage.xaml.cs
+++ CowTown/MainPage.xaml.cs
@@ -1,23 +1,31 @@
-﻿namespace CowTown;
+﻿using System.Collections.Generic;
+
+namespace CowTown;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 	string currentWord = "";
+    List<char> lettersGiven = new List<char>();
+    List<string> wordsSubmitted = new List<string>();
 
 	public MainPage()
 	{
 		InitializeComponent();
-        for (int i = 0; i < 8; i++)
-		{
-			Button letterButton = new Button();
-			letterButton.Text = "A";
-			letterButton.FontSize = 24;
+        lettersGiven = CowTown.LetterUtils.GiveSevenLetters();
+        GenerateLetters();
+    }
+
+	private void GenerateLetters()
+	{
+        EligibleLettersGrid.Children.Clear();
+        lettersGiven.ForEach((letter) =>
+        {
+            Button letterButton = new Button();
+            letterButton.Text = letter.ToString();
+            letterButton.FontSize = 24;
             letterButton.Clicked += OnEligibleLetterClicked;
             EligibleLettersGrid.Children.Add(letterButton);
-        }
-        
-
+        });
     }
 
 	private void OnEligibleLetterClicked(object sender, EventArgs e)
@@ -27,9 +35,10 @@ public partial class MainPage : ContentPage
 		clickedButton.Clicked += OnChosenLetterClicked;
 		EligibleLettersGrid.Children.Remove(clickedButton);
 		ChosenLettersGrid.Children.Add(clickedButton);
-
+        currentWord += clickedButton.Text;
 		Console.WriteLine(clickedButton.Text);
 	}
+
     private void OnChosenLetterClicked(object sender, EventArgs e)
     {
         Button clickedButton = (Button)sender;
@@ -37,12 +46,33 @@ public partial class MainPage : ContentPage
         clickedButton.Clicked -= OnChosenLetterClicked;
         ChosenLettersGrid.Children.Remove(clickedButton);
         EligibleLettersGrid.Children.Add(clickedButton);
+        string newWord = "";
+        bool letterSeen = false;
+        foreach(char c in currentWord)
+        {
+            if(c != clickedButton.Text[0] || letterSeen)
+            {
+                newWord += c;
+            }
+            else
+            {
+                letterSeen = true;
+            }
+        }
+        currentWord = newWord;
+        Console.WriteLine(currentWord);
 
-        Console.WriteLine(clickedButton.Text);
     }
 
     private void OnSubmitWordClicked(object sender, EventArgs e)
 	{
+        wordsSubmitted.Add(currentWord);
+        currentWord = "";
+        foreach (Button b in ChosenLettersGrid.Children)
+        {
+            EligibleLettersGrid.Children.Add(b);
+        }
+        ChosenLettersGrid.Children.Clear();
 	}
 }
 
